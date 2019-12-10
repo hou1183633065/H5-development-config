d67e543d504808c1ea2d969c8d80116676a9f4a1
houzhiqiang <zhq.hou@muheda.com>
2019-12-10 11:33:07 +0800

    代码检测修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,24 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
+
+  extends: ["plugin:vue/recommended", "@vue/prettier"],
+
   parserOptions: {
     parser: "babel-eslint"
+  },
+
+  rules: {
+    "no-console": "off",
+    "no-debugger": "error",
+    "vue/no-async-in-computed-properties": "warning",
+    "vue/no-dupe-keys": "off",
+    "no-unused-vars": "off",
+    "vue/require-default-prop": "off",
+    "no-inner-declarations": "off",
+    "no-undef": "off"
   }
 };
